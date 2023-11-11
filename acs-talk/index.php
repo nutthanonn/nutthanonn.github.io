99c4b0709fb82a3b1c723f0a7cba3ec62a0e8326
nutthanon <nutthanon.tho@gmail.com>
2023-11-11 22:44:48 +0700

    feat: command injection

--- acs-talk/index.php
+++ acs-talk/index.php
@@ -1,10 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="style.css">
+<head>
+    <title>Ping Pong Enterprise</title>
+</head>
+<body>
+<div id='box'>
+    <form id="myForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <label for="inputString">Enter a url to test connection: </label>
+        <input type="text" id="inputString" name="url">
+        <input type="submit" value="Submit">
+    </form>
+    
+    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $value = $_POST['url'] ?? '';
 
-$file = $_GET['file'];
-
-if(isset($file)){
-    include("pages/$file");
-}else{
-    header("Location: index.php?file=home.html");
+    if (strpos($value, ' ') !== false) {
+        echo "<p>URL cannot contain spaces</p>";
+    }else{
+        if (!empty($value)) {
+            $ping = "ping -c 1 " . $value;
+            $result = shell_exec($ping);
+            
+            echo "<p><b>Ping result</b> $result</p>";
+        } else {
+            echo "<p>Please enter a valid URL</p>";
+        }
+    }
 }
 ?>
+</div>
+<div class='ping-pong'></div>
+<script>
+function submitForm() {
+    document.getElementById("myForm").submit();
+}
+</script>
+</body>
+</html>
